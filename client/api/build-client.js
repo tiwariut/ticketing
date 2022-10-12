39f2f776ffd1ef92d74106c3bcf75637642def40
Utkarsh Tiwari <byte-knight@Utkarshs-MacBook-Air.local>
2022-10-12 23:23:51 +0530

    Update hostname and other settings

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,8 @@ export default ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://wwww.ticketing-app-prod.click/',
+      // 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers
     });
   } else {
